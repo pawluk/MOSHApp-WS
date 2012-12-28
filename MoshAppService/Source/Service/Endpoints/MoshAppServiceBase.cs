0a081a8a765b3be99ad0b4940f12433cf645d93c
Jason Recillo <jaysan1292@gmail.com>
2012-12-28 13:18:50 -0500

    MoshAppServiceBase docs

--- MoshAppService/Source/Service/Endpoints/MoshAppServiceBase.cs
+++ MoshAppService/Source/Service/Endpoints/MoshAppServiceBase.cs
@@ -13,6 +13,10 @@ using ServiceStack.ServiceInterface.Auth;
 
 namespace MoshAppService.Service.Endpoints {
     public abstract class MoshAppServiceBase : ServiceStack.ServiceInterface.Service {
+        /// <summary>
+        /// Obtains the user session from either the query string or a supplied cookie. If neither
+        /// exists, create a new session.
+        /// </summary>
         public new IAuthSession Session {
             get {
                 try {
@@ -24,8 +28,19 @@ namespace MoshAppService.Service.Endpoints {
             }
         }
 
+        /// <summary>
+        /// Returns true if the user has successfully logged in.
+        /// </summary>
         public bool IsLoggedIn { get { return Session.IsAuthenticated; } }
+
+        /// <summary>
+        /// Returns the user ID associated with the current session.
+        /// </summary>
         public long UserId { get { return long.Parse(Session.Roles.Find(x => x.Contains("User")).Substring("User ".Length)); } }
+
+        /// <summary>
+        /// Returns the team ID of the team that the current user belongs to.
+        /// </summary>
         public long TeamId { get { return long.Parse(Session.Roles.Find(x => x.Contains("Team")).Substring("Team ".Length)); } }
 
         protected HttpError UnauthorizedResponse(string errorCode = null) {
