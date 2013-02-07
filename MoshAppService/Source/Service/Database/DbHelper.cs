a930f4b8abbbd9ac93dbc65511e5cad69e44725e
Jason Recillo <jaysan1292@gmail.com>
2013-02-07 00:16:32 -0500

    [DebuggerHidden] for fun and profit

--- MoshAppService/Source/Service/Database/DbHelper.cs
+++ MoshAppService/Source/Service/Database/DbHelper.cs
@@ -5,31 +5,37 @@
 
 using System;
 using System.Configuration;
+using System.Diagnostics;
 
 using MySql.Data.MySqlClient;
 
 namespace MoshAppService.Service.Database {
     public static class DbHelper {
+        [DebuggerHidden]
         public static MySqlConnection CreateConnection() {
             return new MySqlConnection(ConfigurationManager.ConnectionStrings["MoshConnection"].ConnectionString);
         }
 
+        [DebuggerHidden]
         public static MySqlConnection OpenConnection() {
             var connection = CreateConnection();
             connection.Open();
             return connection;
         }
 
+        [DebuggerHidden]
         public static void CloseConnection(MySqlConnection connection) {
             if (connection != null) connection.Close();
         }
 
+        [DebuggerHidden]
         public static MySqlConnection OpenConnectionAndBeginTransaction(out MySqlTransaction transaction) {
             var connection = OpenConnection();
             transaction = connection.BeginTransaction();
             return connection;
         }
 
+        [DebuggerHidden]
         public static void CloseConnectionAndEndTransaction(MySqlConnection connection, MySqlTransaction transaction, MySqlDataReader reader) {
             if (reader != null) reader.Close();
             if (transaction != null) transaction.Commit();
--- MoshAppService/Source/Utils/StringExtensions.cs
+++ MoshAppService/Source/Utils/StringExtensions.cs
@@ -4,20 +4,23 @@
 // Author: Jason Recillo
 
 using System;
+using System.Diagnostics;
 
 using JetBrains.Annotations;
 
 namespace MoshAppService.Utils {
     public static class StringExtensions {
-        [NotNull, StringFormatMethod("str")]
+        [DebuggerHidden, NotNull, StringFormatMethod("str")]
         public static string F([NotNull] this string str, [NotNull] params object[] args) {
             return String.Format(str, args);
         }
 
+        [DebuggerHidden]
         public static bool IsNullOrEmpty(this string input) {
             return string.IsNullOrEmpty(input);
         }
 
+        [DebuggerHidden]
         public static bool IsNotNullOrEmpty(this string input) {
             return !string.IsNullOrEmpty(input);
         }
