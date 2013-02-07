1a5b281eafc6f87d1b818d057d73274311bc7873
Jason Recillo <jaysan1292@gmail.com>
2013-02-07 00:15:12 -0500

    Moved IDbProvider to its own file

--- MoshAppService/Source/Service/Database/BaseDbProvider.cs
+++ MoshAppService/Source/Service/Database/BaseDbProvider.cs
@@ -10,11 +10,6 @@ using MoshAppService.Service.Data;
 using MySql.Data.MySqlClient;
 
 namespace MoshAppService.Service.Database {
-    public interface IDbProvider<out T> where T : Entity<T> {
-        // Indexer (i.e., var item = dbProvider[id];)
-        T this[long id] { get; }
-    }
-
     public abstract class BaseDbProvider<T> : IDbProvider<T> where T : Entity<T> {
         public abstract T this[long id] { get; }
         protected abstract T BuildObject(MySqlDataReader reader);
---
+++ MoshAppService/Source/Service/Database/IDbProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+using MoshAppService.Service.Data;
+
+namespace MoshAppService.Service.Database {
+    public interface IDbProvider<out T> where T : Entity<T> {
+        // Indexer (i.e., var item = dbProvider[id];)
+        T this[long id] { get; }
+    }
+}
