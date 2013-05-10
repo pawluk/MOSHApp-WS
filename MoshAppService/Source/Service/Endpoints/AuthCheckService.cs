4886ad62da8d10667cee68cb842a862e971f3a92
Jason Recillo <jaysan1292@gmail.com>
2013-05-10 03:13:08 -0400

    Added authorization check

---
+++ MoshAppService/Source/Service/Endpoints/AuthCheckService.cs
@@ -0,0 +1,22 @@
+﻿// Project: MoshAppService
+// Filename: AuthCheckService.cs
+// 
+// Author: Jason Recillo
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using ServiceStack.ServiceHost;
+
+namespace MoshAppService.Service.Endpoints {
+    [Route("/authenticate/check", "GET")]
+    public class AuthCheck { }
+
+    public class AuthCheckService : MoshAppServiceBase {
+        public object Get(AuthCheck request) {
+            return IsLoggedIn;
+        }
+    }
+}
